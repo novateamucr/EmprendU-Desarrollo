7b915eaf67516633f9c31a3ba7bb3cf46905ef5a
ValeGS1202 <valegs1202@gmail.com>
2025-09-04 23:19:51 -0600

    ajustes ruta a profile

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import QueryProvider from './context/QueryProvider';
 import Home from './routes/Home';
-import { Perfil } from './routes/Perfil';
+import { Perfil } from './routes/Profile';
 import { EditarPerfil } from './routes/EditarPerfil';
 import LandingPage from './routes/Landing';
 import { FeedEmprendimiento } from './routes/FeedEmpredimiento';
@@ -18,7 +18,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/perfil" element={<Perfil />} />
+          <Route path="/profile" element={<Perfil />} />
           <Route path="/perfil/editar" element={<EditarPerfil />} /> 
           <Route path="/landing" element={<LandingPage />} />
           <Route path="/feed/emprendimiento" element={<FeedEmprendimiento />} />
