ceda014d1d1ea8d79c109d53a7e29f54a2a7bff9
ValeGS1202 <valegs1202@gmail.com>
2025-10-02 13:47:22 -0600

    funcionalidad para guardar las valoraciones

---
+++ app/Http/Controllers/ReviewController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Review;
+
+class ReviewController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $reviews = Review::with(['user', 'product'])->get();
+        return response()->json($reviews);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+         $data = $request->validate([
+            'rating' => 'required|integer|min:1|max:5',
+            'review' => 'nullable|string',
+            'user_id' => 'nullable|exists:users,id',
+            'product_id' => 'nullable|exists:products,id',
+        ]);
+
+        $review = Review::create($data);
+
+        return response()->json([
+            'message' => 'Reseña creada exitosmente',
+            'review' => $review->load(['user', 'product']),
+        ], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $review = Review::with(['user', 'product'])->findOrFail($id);
+        return response()->json($review);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $review = Review::findOrFail($id);
+
+        $data = $request->validate([
+            'rating' => 'sometimes|integer|min:1|max:5',
+            'review' => 'nullable|string',
+            'user_id' => 'nullable|exists:users,id',
+            'product_id' => 'nullable|exists:products,id',
+        ]);
+
+        $review->update($data);
+
+        return response()->json([
+            'message' => 'Review actualizado correctamente',
+            'review' => $review->load(['user', 'product']),
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $review = Review::findOrFail($id);
+        $review->delete();
+
+        return response()->json(['message' => 'Reseña eliminada correctamente']);
+    }
+}
