26fc89364abab24f5bda6b169c702a169ea41c18
Erickaflv <erickaflv7@gmail.com>
2025-10-09 21:43:11 -0600

    Ver detalles de feria

--- src/components/FeriaCard.tsx
+++ src/components/FeriaCard.tsx
@@ -11,16 +11,16 @@ interface FeriaCardProps {
 export function FeriaCard(props: FeriaCardProps) {
   return (
     <div className="max-w-sm p-4 bg-white rounded-2xl shadow-md overflow-hidden hover:shadow-lg transition">
-      {/* Imagen */}
+      
       <img
         className="w-full h-48 object-cover rounded-2xl"
         src={props.imgUrl}
         alt={props.title}
       />
 
-      {/* Contenido */}
+     
       <div className="p-4">
-        {/* 🔹 Se fija altura mínima para alinear títulos */}
+       
         <h3 className="text-gray-900 font-medium text-lg mb-4 min-h-[56px] flex items-center">
           {props.title}
         </h3>
@@ -30,9 +30,10 @@ export function FeriaCard(props: FeriaCardProps) {
         <div className="flex justify-between text-sm text-gray-600 mb-4">
           <span>{props.location}</span>
           <span>{props.time}</span>
+          
         </div>
 
-        {/* Botón */}
+       
         <button
           onClick={props.onButtonClick}
           className="mt-4 w-full bg-black text-white py-2 rounded-full hover:bg-gray-800 transition"
