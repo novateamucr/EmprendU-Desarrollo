ab96521240f2b7c9d972f0fb5467ad4bef9b269d
ValeGS1202 <valegs1202@gmail.com>
2025-10-13 21:46:22 -0600

    arreglo de scroll en FAQs

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -2,15 +2,15 @@ import React from 'react';
 
 const Footer: React.FC = () => {
   return (
-    <footer className="w-full mb-5 mt-10">
-      <div className="flex justify-center items-center flex-wrap gap-6 text-base font-normal text-gray-500">
+    <footer className="w-full mb-5 mt-5">
+      <div className="flex justify-center items-center flex-wrap gap-2 font-normal text-gray-500 text-sm">
         <span>© 2025 EmprendU. Todos los derechos reservados.</span>
         <div className=" h-6 border-l border-gray-400 mx-2" />
-        <a href="/faqs" className="hover:underline text-gray-500">Preguntas frecuentes</a>
+        <a href="/faqs" className="hover:underline text-gray-500 text-sm">Preguntas frecuentes</a>
         <div className="h-6 border-l border-gray-400 mx-2" />
-        <a href="/contactUs" className="hover:underline  text-gray-500">Contáctanos</a>
+        <a href="/contactUs" className="hover:underline text-gray-500 text-sm">Contáctanos</a>
         <div className="h-6 border-l border-gray-400 mx-2" />
-        <a href="/" className="hover:underline text-gray-500">EmprendU</a>
+        <a href="/" className="hover:underline text-gray-500 text-sm">EmprendU</a>
       </div>
     </footer>
   );
