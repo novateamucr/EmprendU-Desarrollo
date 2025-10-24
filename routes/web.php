a827b5a52cf5a02cb5adbf6982eef9872edafe0c
LuanaSalasA <luana.salas.a@gmail.com>
2025-10-23 20:44:09 -0600

    Endpoints html para metadatos

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ShareController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Share endpoints (HTML with Open Graph metadata for scrapers)
+Route::get('/share/product/{id}', [ShareController::class, 'product'])->name('share.product');
